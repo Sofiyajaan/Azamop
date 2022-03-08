606bc17508b04900744105106cddaff90f7d338b
Adarshtiwari1305 <96828009+Adarshtiwari1305@users.noreply.github.com>
2022-03-08 10:14:14 +0530

    Update song.py

--- core/song.py
+++ core/song.py
@@ -42,7 +42,7 @@ class Song:
         elif isinstance(link, dict):
             self.title: str = "Live Stream"
             self.duration: str = None
-            self.thumb: str = "https://telegra.ph/file/820cac7cb7b1a025542e2.jpg"
+            self.thumb: str = "https://https://telegra.ph/file/799bc7e31a3214946df5d.jpg"
             self.remote_url: str = link["url"]
             self.yt_url: str = link["url"]
             self.headers: dict = None
